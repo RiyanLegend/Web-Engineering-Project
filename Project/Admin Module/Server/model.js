54acf21f5f8e38a50400e7c7ed84278143c4e483
Haseeb Ur Rehman <78302806+Haseeb0951@users.noreply.github.com>
2022-05-27 16:13:46 +0500

    edited schema
    edited schema

--- Project/Admin Module/Server/model.js
+++ Project/Admin Module/Server/model.js
@@ -5,6 +5,18 @@ var schema = new mongoose.Schema({
         type : String,
         required: true
     },
+    password: {
+    type: String,
+    required: true
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  resetLink: {
+    type: String,
+    default: ''
+  }
     email : {
         type: String,
         required: true,
